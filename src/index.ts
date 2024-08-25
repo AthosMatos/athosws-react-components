f328510e9b671c6f14e2140f58125dbc5c89995b
Athos Matos <ladiesman217.as@gmail.com>
2024-08-24 22:03:13 -0300

    reestructuring the folders

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export { ATHOSButton } from "./DefaultComponents/ATHOSButton";
-
-export { ATHOSInput } from "./DefaultComponents/ATHOSInput";
-
-export { ATHOSResizableDiv } from "./DefaultComponents/ATHOSResizableDiv";
-export { ATHOSSideMenu } from "./DefaultComponents/ATHOSSideMenu";
-export { ATHOSTooltip } from "./DefaultComponents/ATHOSTooltip";
