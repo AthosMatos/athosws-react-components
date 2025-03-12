c93035cc51a240743cecff0b21ea0c37e80535a0
Athos Matos <ladiesman217.as@gmail.com>
2025-03-12 14:03:51 -0300

    performace testing table

--- src/Module/ATHOSDynamicTable/component/Sections/ADTCells/ADTCellV2/index.tsx
+++ src/Module/ATHOSDynamicTable/component/Sections/ADTCells/ADTCellV2/index.tsx
@@ -1,11 +1,21 @@
 import { ReactNode } from "react";
+import { ATHOSTooltip } from "../../../../../ATHOSTooltip";
+import { ADTCellColWrapper } from "../../../styled";
 
 interface ADTCellColumnV2Props {
   value?: ReactNode;
 }
 
 const ADTCellColumnV2 = ({ value }: ADTCellColumnV2Props) => {
-  return <td className="">{value}</td>;
+  return (
+    <ADTCellColWrapper>
+      {
+        <ATHOSTooltip followCursor content={value}>
+          {(ref) => <div ref={ref}>{value}</div>}
+        </ATHOSTooltip>
+      }
+    </ADTCellColWrapper>
+  );
 };
 
 export default ADTCellColumnV2;
