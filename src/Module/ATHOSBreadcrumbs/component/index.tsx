d1b260f6612fca46f00f356e417c8dd7540babea
Athos Matos <ladiesman217.as@gmail.com>
2025-05-25 11:49:19 -0300

    crumbs testing

--- src/Module/ATHOSBreadcrumbs/component/index.tsx
+++ src/Module/ATHOSBreadcrumbs/component/index.tsx
@@ -1,46 +1,73 @@
-import { FaHouse, FaCaretRight } from "react-icons/fa6";
-import { PiCaretRight } from "react-icons/pi";
-import { ATHOSDropDown } from "../../ATHOSDropDown/component";
 import { useState } from "react";
+import { FaHouse } from "react-icons/fa6";
+import { PiCaretRightBold } from "react-icons/pi";
+import { ATHOSDropDown } from "../../ATHOSDropDown/component";
+
+const crumbs = [
+  {
+    crumb: <FaHouse />,
+  },
+
+  {
+    crumb: "Gerenciamento",
+    dropdown: [
+      {
+        label: "Dashboard do Procurador",
+        value: 0,
+        path: "/athos/dashboard",
+      },
+      {
+        label: "Dashboard RPV",
+        value: 1,
+        path: "/athos/dashboard/rpv",
+      },
+    ],
+  },
+];
 
 const ATHOSBreadcrumbs = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [selectedOpt, setSelectedOpt] = useState<number>();
+  const [selectedOpt, setSelectedOpt] = useState<{ value: number; label: string } | undefined>();
   return (
-    <div className="flex items-center text-zinc-400">
-      <FaHouse />
-      <PiCaretRight className="text-lg" />
-      <ATHOSDropDown
-        onToggle={(isOpen) => {
-          setIsOpen(isOpen);
-        }}
-        className={`bg-zinc-100 border-zinc-300 border border-t-0 px-1 py-1 !rounded-tl-none`}
-        spacing={0}
-        buttonClassName={`${isOpen ? "bg-zinc-100 px-3 font-medium  border-zinc-300 border border-b-0" : ""} py-1 ${
-          isOpen ? "rounded-t-lg" : "rounded-lg"
-        }`}
-        labelClassName="px-2 py-1 rounded-md border font-medium"
-        labels={[
-          {
-            label: "Dashboard do Procurador",
-            onClick: () => {
-              setSelectedOpt(0);
-            },
-            className: `${selectedOpt === 0 ? "bg-white border-zinc-300 text-zinc-700" : "border-transparent"}  `,
-          },
-          {
-            label: "Dashboard RPV",
-            onClick: () => {
-              setSelectedOpt(1);
-            },
-            className: `${selectedOpt === 1 ? "bg-white border-zinc-300 text-zinc-700" : "border-transparent"} `,
-          },
-        ]}
-      >
-        Gerênciamento
-      </ATHOSDropDown>
-      <PiCaretRight className="text-lg" />
-      <p className="font-bold text-3xl text-zinc-700">Dashboard do Procurador</p>
+    <div className="flex items-center text-zinc-400 gap-1">
+      {crumbs.map((item, index) => {
+        return (
+          <>
+            {item.dropdown ? (
+              <ATHOSDropDown
+                key={index}
+                onToggle={(isOpen) => {
+                  setIsOpen(isOpen);
+                }}
+                className={`bg-zinc-100 overflow-hidden outline-zinc-300 outline outline-1 px-1 py-1 !rounded-tl-none`}
+                spacing={0}
+                buttonClassName={`transition-[padding] outline outline-1 outline-b-0 ${
+                  isOpen ? "bg-zinc-100 outline-zinc-300 px-2" : "outline-transparent"
+                } py-1 ${isOpen ? "rounded-t-lg" : "rounded-lg"}`}
+                labelClassName="px-2 py-1 rounded-md outline-1 outline"
+                labels={item.dropdown.map((opt) => {
+                  return {
+                    label: opt.label,
+                    onClick: () => {
+                      setSelectedOpt(opt);
+                    },
+                    className: `${selectedOpt?.value === opt.value ? "bg-white outline-zinc-300 text-zinc-700" : "outline-transparent"}  `,
+                  };
+                })}
+              >
+                {item.crumb}
+              </ATHOSDropDown>
+            ) : (
+              item.crumb
+            )}
+
+            <div className="flex items-center gap-2">
+              <PiCaretRightBold className="text-lg" />
+            </div>
+          </>
+        );
+      })}
+      <p className="font-bold text-3xl text-zinc-700">{selectedOpt?.label}</p>
     </div>
   );
 };
